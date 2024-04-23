2f9bef460b618b68861b90d9d0fcb003c0493152
screamzombie <53398003+screamzombie@users.noreply.github.com>
2024-04-23 16:58:06 +0800

    update some comment

--- alpha.py
+++ alpha.py
@@ -7,20 +7,24 @@ stopwords = ['a', 'b', 'c', 'd']
 
 
 class controller:
-    def __init__(self):  # * 初始化
+    def __init__(self):
         self.stop_words = []
         self.paper_list = []
         self.root_data_path = "./data"
         self.read_data()
 
-    
-    def return_paper_all_content(self, index):  # * 得到对应索引的内容
+    def storage_words_freq_by_year(self, year):
+        for paper in self.paper_list:
+            if paper.year == str(year):
+                pass
+
+    def return_paper_all_content(self, index):  # * get total content by index
         if index <= len(self.paper_list):
             return self.paper_list[index].all_content
         else:
             print("index out of range")
 
-    def show_all_years_freq(self): # * 获得所有年份的词汇频率ï
+    def show_all_years_freq(self):  # * display all previous years words freq
         total_content = ""
         for exam in self.paper_list:
             total_content += exam.all_content
@@ -34,7 +38,7 @@ class controller:
         for word, freq in example:
             print(word, ":", freq)
 
-    def word_frequency_analysis(self, content):  # * 计算词频
+    def word_frequency_analysis(self, content):  # * calculate freq
         words = re.findall(r'\w+', content.lower())
         words_clean = []
         for word in words:
@@ -48,13 +52,13 @@ class controller:
             else:
                 word_freq[word] = 1
 
-        # 按词频降序排序
+        # sort by frequency down
         sorted_freq = sorted(
             word_freq.items(), key=lambda x: x[1], reverse=False)
-        return sorted_freq  # * 返回词频字典
+        return sorted_freq
 
     def read_data(self):
-        files = os.listdir(self.root_data_path)  # * 得到文件夹下的所有文件名称
+        files = os.listdir(self.root_data_path)
         files.sort()
         print(files)
         for i in files:
@@ -91,25 +95,26 @@ class controller:
 class paper:
     def __init__(self):
         self.year = None
-        self.read_content_a = None  # * 阅读1
-        self.read_content_b = None  # * 阅读2
-        self.read_content_c = None  # * 阅读3
-        self.read_content_d = None  # * 阅读4
-        self.complete_content = None  # * 完形
-        self.translate_content = None  # * 翻译
-        self.all_content = ""  # * 整张试卷的所有内容
-        self.dict_freq_all = {}  # * 所有内容的频率字典
-        self.dict_freq_read = {}  # * 仅仅是阅读的频率
+        self.read_content_a = None  # * read 1
+        self.read_content_b = None  # * read 2
+        self.read_content_c = None  # * read 3
+        self.read_content_d = None  # * read 4
+        self.complete_content = None  # * complate
+        self.translate_content = None  # * translate
+        self.all_content = ""  # * all content of this paper
+        self.dict_freq_all = {}  # * a dict of all words freq
+        self.dict_freq_read = {}  # * only read texts
 
-    def join_all_content(self):  # * 把所有内容合并到一起
+    def join_all_content(self):  # * join all content
         self.all_content = self.read_content_a + self.read_content_b + \
             self.read_content_c + self.read_content_d + \
             self.complete_content + self.translate_content
-        # print(self.all_content)
+
+    # * return this paper's all words freq
+    def return_this_paper_all_words_freq(self):
+        pass
 
 
 if __name__ == "__main__":
     c = controller()
-    # print(len(c.paper_list[0].all_content))
-    # print(len(c.paper_list[2].all_content))
     c.show_all_years_freq()
