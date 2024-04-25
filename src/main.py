8e0514f8e66768107062b6ce556db92bdee05fd3
screamzombie <53398003+screamzombie@users.noreply.github.com>
2024-04-25 21:25:59 +0800

    None

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
-
+from calculate import *
 
 if __name__ == '__main__':
-    pass
+    c = controller()
+    c.load_paper()
+    c.calculate_freq_dict()
+    c.storage_all_content_words_freq()
+    c.storage_words_freq_by_paper()
+    c.create_paper_directory()
+    c.storage_sections_words_freq()
