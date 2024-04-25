0d016ddad4848f296ff61df396e6fad6cdf8bc67
screamzombie <53398003+screamzombie@users.noreply.github.com>
2024-04-25 22:52:36 +0800

    finish base ui

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
+from panel import *
 from calculate import *
 
 if __name__ == '__main__':
     c = controller()
     c.default_run()
-    # c.clear_all_temp_storage()
+    app = QApplication(sys.argv)  
+    myWin = windows()
+    # 将窗口控件显示在屏幕上
+    myWin.show()
+    # 程序运行，sys.exit方法确保程序完整退出。
+    sys.exit(app.exec_())
+
--- src/panel.py
+++ src/panel.py
@@ -1,6 +1,34 @@
-import pandas as pd
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from calculate import *
+from squeeze_panel.Ui_form import *
+
+
+class dash_board(QMainWindow, Ui_base_Widget):
+    def __init__(self, parent=None):
+        super(dash_board, self).__init__(parent)
+        self.controller = controller()  # * 导入数据分析模块
+        self.setupUi(self)
+        self.my_init()
+        self.make_data_pushButton.clicked.connect(self.make_data)
+
+    def make_data(self):
+        self.controller.default_run()
+
+    def my_init(self):
+        csvFiles = os.listdir("../data/tempData")
+        csvFiles.sort()
+        for csvFile in csvFiles:
+            if csvFile.endswith("_words_freq.csv"):
+                self.words_comboBox.addItem(csvFile)
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('result/all_years_words_freq.csv',header=None,sep=' ',nrows=30)
-    print(df.values.tolist())
+    app = QApplication(sys.argv)
+
+    myWin = dash_board()
+    # 将窗口控件显示在屏幕上
+    myWin.show()
+    # 程序运行，sys.exit方法确保程序完整退出。
+    sys.exit(app.exec_())
